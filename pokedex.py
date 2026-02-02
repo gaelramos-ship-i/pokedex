8c25a3a2471898a302bc932d1d1910ede023e3d9
gaelramos-ship-i <gael.ramos@laplateforme.io>
2026-02-02 10:06:42 +0100

    ajout class, list, label

--- pokedex.py
+++ pokedex.py
@@ -4,6 +4,24 @@ fenetre = tk.Tk()
 fenetre.title("Pokedex")
 fenetre.geometry("680x480")
 
+class Pokemon:
+    def __init__(self, name, type, capacity):
+        self.name = name
+        self.type = type
+        self.capacity = capacity
 
+    def show_informations(self):
+        print(f"Nom : {self.name}")
+        print(f"Type : {self.type}")
+        print(f"Capacité : {self.capacity}")
+
+pokemon = Pokemon("Pikachu", "Electrique", "statik")
+
+
+list_pokemon = tk.Listbox(fenetre)
+list_pokemon.pack()
+
+label_info = tk.Label(fenetre, text="Pokemon ?")
+label_info.pack()
 
 fenetre.mainloop()
