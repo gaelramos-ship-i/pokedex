2a44eec21fa178d6fa7083d2c685d91f4595463c
gaelramos-ship-i <gael.ramos@laplateforme.io>
2026-02-02 11:37:28 +0100

    ajustement d'affichage

--- pokedex.py
+++ pokedex.py
@@ -10,20 +10,26 @@ class Pokemon:
         self.type = type
         self.capacity = capacity
 
-pokemon1 = Pokemon("Bulbizarre", "Plante, Poison", "Capacités : Engrais, Chlorophyle")
-pokemon2 = Pokemon("Salameche", "Feu", "Capacités : Brasier, Force Soleil")
+pokemon1 = Pokemon("Bulbizarre", "Plante, Poison", "Engrais, Chlorophyle")
+pokemon2 = Pokemon("Salameche", "Feu", "Brasier, Force Soleil")
 pokemon3 = Pokemon("Pikachu", "Electrique", "Statik, Paratonnerre")
 
 def show_pokemon():
     selection = list_pokemon.curselection()
     if selection:
         if selection[0] == 0:
-            label_info.config(text=f"{pokemon1.name} est un pokemon de type {pokemon1.type}, capacités : {pokemon1.capacity}")
-        if selection[0] == 1:
-            label_info.config(text=f"{pokemon2.name} est un pokemon de type {pokemon2.type}, capacités : {pokemon2.capacity}")
-        if selection[0] == 2:
-            label_info.config(text=f"{pokemon3.name} est un pokemon de type {pokemon3.type}, capacités : {pokemon3.capacity}")
+            label_info.config(text=f"{pokemon1.name} est un pokemon de type {pokemon1.type},")
+            label_capacity.config(text=f"Capacités : {pokemon1.capacity}")
 
+        if selection[0] == 1:
+            label_info.config(text=f"{pokemon2.name} est un pokemon de type {pokemon2.type},")
+            label_capacity.config(text=f"Capacités : {pokemon2.capacity}")
+
+        if selection[0] == 2:
+            label_info.config(text=f"{pokemon3.name} est un pokemon de type {pokemon3.type},")
+            label_capacity.config(text=f"Capacités : {pokemon3.capacity}")
+    else:
+        label_info.config(text="Il faut choisir un Pokemon !")
 
 list_pokemon = tk.Listbox(fenetre)
 list_pokemon.pack()
@@ -32,12 +38,13 @@ list_pokemon.insert(tk.END, "Bulbizarre")
 list_pokemon.insert(tk.END, "Salameche")
 list_pokemon.insert(tk.END, "Pikachu")
 
-
-
 button = tk.Button(fenetre, text="Affiche le pokemon", command=show_pokemon)
 button.pack()
 
 label_info = tk.Label(fenetre, text="Pokemon ?")
 label_info.pack()
+
+label_capacity = tk.Label(fenetre, text="")
+label_capacity.pack()
 
 fenetre.mainloop()
